95bdeac2ebcc243ce1ad1a27bd18068582073959
jermb <77866654+jermb@users.noreply.github.com>
2023-03-10 13:54:21 -0600

    Added create_line_graph function

--- graph.py
+++ graph.py
@@ -10,10 +10,13 @@ data = [{"date": k, **v} for k, v in json["Time Series (5min)"].items()]
 print(data[0])
 
 
-
-date_string = '2023-03-09 11:05:00'
-
-
+options = {
+    "Open": [],
+    "High": [],
+    "Low": [],
+    "Close": [],
+    "Volume": []
+}
 
 #   Data as a JSON Obj
 def create_line_graph(data):
@@ -21,33 +24,23 @@ def create_line_graph(data):
         x_label_rotation=35, truncate_label=-1,
         x_value_formatter=lambda dt: dt.strftime('%d, %b %Y at %I:%M:%S %p')
     )
+    line = append_data(data, options, line)
+    line.render_in_browser()
 
-    data["Time Series (5min)"]
-    
-    open = []
-    high = []
-    low = []
-    close = []
-    volumne = []
-
-
+def append_data(data, options, graph):
     for item in data:
-        line.add("")
+        date = string_to_datetime(item["date"])
+        options["Open"].append((date, float(item["1. open"])))
+        options["High"].append((date, float(item["2. high"])))
+        options["Low"].append((date, float(item["3. low"])))
+        options["Close"].append((date, float(item["4. close"])))
+        options["Volume"].append((date, float(item["5. volume"])))
+    
+    for k, v in options:
+        graph.add(k, v)
 
-    line.add("Serie", [
-        (datetime(2013, 1, 2, 12, 0), 300),
-        (datetime(2013, 1, 12, 14, 30, 45), 412),
-        (datetime(2013, 2, 2, 6), 823),
-        (datetime(2013, 2, 22, 9, 45), 672)
-    ])
-
-
-
+#   Converts date string to datetime object
 def string_to_datetime(date):
     date_format = '%Y-%m-%d %H:%M:%S'
     return datetime.strptime(date, date_format)
-
-
-
-
 
