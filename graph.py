8cf9a0d6c8e97dbc141c60f4b16162e97e10ecc8
jermb <77866654+jermb@users.noreply.github.com>
2023-03-10 21:10:04 -0600

    Simplification and bug fixing

--- graph.py
+++ graph.py
@@ -7,57 +7,52 @@ from datetime import datetime
 json = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo").json()
 data = [{"date": k, **v} for k, v in json["Time Series (5min)"].items()]
 
-print(data[0])
-
 
 options_template = {
     "Open": [],
     "High": [],
     "Low": [],
-    "Close": [],
-    "Volume": []
+    "Close": []
 }
 
-#   Data as a JSON Obj
-def append_data(data, graph):
+def create_line_graph(data):
+    create_graph(data, pygal.Line())
+
+def create_bar_graph(data):
+    create_graph(data, pygal.Bar())
+
+#   Data as a JSON Obj/Dictionary
+def create_graph(data, graph):
     options = options_template.copy()
+    dates = []
+    data.sort(key = get_date)
+
+    #   Format data from JSON to be used by graph
     for item in data:
-        date = string_to_datetime(item["date"])
-        options["Open"].append((date, float(item["1. open"])))
-        options["High"].append((date, float(item["2. high"])))
-        options["Low"].append((date, float(item["3. low"])))
-        options["Close"].append((date, float(item["4. close"])))
-        options["Volume"].append((date, float(item["5. volume"])))
+        dates.append(string_to_datetime(item["date"]))
+        options["Open"].append(float(item["1. open"]))
+        options["High"].append(float(item["2. high"]))
+        options["Low"].append(float(item["3. low"]))
+        options["Close"].append(float(item["4. close"]))
 
-    print(options["Volume"])
-
+    #   Add Data to graph
     for opt in options:
         graph.add(opt, options[opt])
-    return graph
+
+    #   Graph Setup and Render
+    graph.x_label_rotaion = 20
+    graph.x_labels = dates
+    graph.render_in_browser()
 
 #   Converts date string to datetime object
 def string_to_datetime(date):
     date_format = '%Y-%m-%d %H:%M:%S'
     return datetime.strptime(date, date_format)
 
+#   Used to sort the data array by each dict's  "date" key
 def get_date(item):
     return item["date"]
 
-def create_line_graph(data):
-    line = pygal.DateTimeLine(
-        x_label_rotation=35, truncate_label=-1,
-        x_value_formatter=lambda dt: dt.strftime('%d, %b %Y at %I:%M:%S %p')
-    )
-    line = append_data(data, line)
-    line.render_in_browser()
-
-def create_bar_graph(data):
-
-    data.sort(key = get_date)
-
-    line = pygal.Bar()
-    line = append_data(data, line)
-    line.render_in_browser()
-
-# create_bar_graph(data)
-create_line_graph(data)
+#   Test calls
+create_bar_graph(data)
+# create_line_graph(data)
