66481f327f6037ebfc749088e493be59690fc2a8
jermb <77866654+jermb@users.noreply.github.com>
2023-03-14 13:43:44 -0500

    Added doc strings to functions

--- graph.py
+++ graph.py
@@ -15,14 +15,33 @@ options_template = {
     "Close": []
 }
 
-def create_line_graph(data):
+def create_line_graph(data: dict):
+    '''
+    Creates a Line Graph of given data and displays in the user's default browser.
+
+    Parameters:
+        data: A dictionary representation of the data JSON.
+    '''
     create_graph(data, pygal.Line())
 
 def create_bar_graph(data):
+    '''
+    Creates a Bar Graph of given data and displays in the user's default browser.
+
+    Parameters:
+        data: A dictionary representation of the data JSON.
+    '''
     create_graph(data, pygal.Bar())
 
 #   Data as a JSON Obj/Dictionary
 def create_graph(data, graph):
+    '''
+    Creates a graph from the given data and displays it in the user's default browser.
+
+    Parameters:
+        data: A dictionary representation of the data JSON.
+        graph: A pygal graph object (pygal.Bar() & pygal.Line() are known to work)
+    '''
     options = options_template.copy()
     dates = []
     data.sort(key = get_date)
@@ -45,14 +64,26 @@ def create_graph(data, graph):
     graph.render_in_browser()
 
 #   Converts date string to datetime object
-def string_to_datetime(date):
+def string_to_datetime(date: str):
+    '''
+    Converts a string date in format YYYY-MM-DD HH:MM:SS to a datetime object
+
+    Parameters:
+        date: String date in format YYYY-MM-DD HH:MM:SS
+
+    Returns:
+        datetime object
+    '''
     date_format = '%Y-%m-%d %H:%M:%S'
     return datetime.strptime(date, date_format)
 
 #   Used to sort the data array by each dict's  "date" key
-def get_date(item):
+def get_date(item: dict):
+    '''
+    Gets the value of "date" from a dictionary
+    '''
     return item["date"]
 
 #   Test calls
-create_bar_graph(data)
-# create_line_graph(data)
+# create_bar_graph(data)
+create_line_graph(data)
