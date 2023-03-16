302765bbdb25299c9743978137a74535e131f874
Oscar S <ols6vk@umsystem.edu>
2023-03-16 15:23:43 -0500

    Added get_dates() function

--- ui.py
+++ ui.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 def get_stock_symbol():
     stockSymbol = input("Enter the stock Symbol you are looking for: ")
     print(stockSymbol)
@@ -37,6 +38,23 @@ def get_time_series():
             print("Invalid input. Please enter 1, 2, 3 or 4.")
     return timeSeries
 
+def get_dates():
+    while True:
+        try:
+            bDate = input("Enter the start date in format (YYYY-MM-DD): ")
+            bDate = datetime.strptime(bDate, "%Y-%m-%d")
+            print(bDate.strftime("%Y-%m-%d"))
+            eDate = input("Enter the ending date in format (YYYY-MM-DD): ")
+            eDate = datetime.strptime(eDate, "%Y-%m-%d")
+            print(eDate.strftime("%Y-%m-%d"))
+            if bDate > eDate:
+                print("Error: Start date must be before end date.")
+            else:
+                return bDate, eDate
+        except ValueError:
+            print("Error: Invalid date format. Please enter a date in the format YYYY-MM-DD.")
+        #if end date is entered that is before start date this function has a bug where it asks for the end date multiple times even
+        #if a correct end date is entered
 def restart_program():
     restart = input("Do you want to restart the program? (y/n): ")
     return restart.lower() == 'y'
@@ -47,6 +65,7 @@ def main():
         stockSymbol = get_stock_symbol()
         chartType = get_chart_type()
         timeSeries = get_time_series()
+        bDate, eDate = get_dates()
 
         if not restart_program():
             break
